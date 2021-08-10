485710c514f8dbec781f05f22a809d2c21413fb4
swlee9087 <sw_lee9087@naver.com>
2021-08-10 17:48:00 +0900

    first commit

---
+++ src/main/java/com/example/mongoJAVA/MongoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongoJAVA;
+//2
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoJavaApplication.class, args);
+	}
+
+}
